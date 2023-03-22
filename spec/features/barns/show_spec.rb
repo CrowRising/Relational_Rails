5b79f0465a3d12f529d5ca96c9d14feb804ba292
Crow Rising <miscbyrd@gmail.com>
2023-03-22 14:57:08 -0600

    test:Add test for horse show spec

--- spec/features/barns/show_spec.rb
+++ spec/features/barns/show_spec.rb
@@ -6,12 +6,10 @@ RSpec.describe '/barns/:id', type: :feature do
   describe 'as a visitor when I visit a barn by the id' do
     let!(:barn_1) { Barn.create!(name: 'Nix', location: 'Nix Lane', arena: true, trail_access: true, monthly_fee: 360, stalls: 24, vacancy: true) }
     let!(:barn_2) { Barn.create!(name: 'Arrowhead', location: 'West Alameda', arena: true, trail_access: false, monthly_fee: 475, stalls: 20, vacancy: false) }
-    let!(:barn_3) { Barn.create!(name: 'Goosedowns', location: 'Airport Road', arena: true, trail_access: false, monthly_fee: 900, stalls: 40, vacancy: true)}
-
-    it 'displays a barn and attributes' do
+    
+    it 'displays barn_1 and attributes' do
       visit "/barns/#{barn_1.id}"
-save_and_open_page
-
+     
       expect(page).to have_content(barn_1.location)
       expect(page).to have_content(barn_1.name)
       expect(page).to have_content(barn_1.arena)
@@ -19,7 +17,18 @@ save_and_open_page
       expect(page).to have_content(barn_1.monthly_fee)
       expect(page).to have_content(barn_1.stalls)
       expect(page).to have_content(barn_1.vacancy)
-      
+    end
+
+    it 'displays a barn_2 and attributes' do
+      visit "/barns/#{barn_2.id}"
+     
+      expect(page).to have_content(barn_2.location)
+      expect(page).to have_content(barn_2.name)
+      expect(page).to have_content(barn_2.arena)
+      expect(page).to have_content(barn_2.trail_access)
+      expect(page).to have_content(barn_2.monthly_fee)
+      expect(page).to have_content(barn_2.stalls)
+      expect(page).to have_content(barn_2.vacancy)
     end
   end
 end
